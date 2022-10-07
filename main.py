a19f20aa7bea5e370548e930d3a6b1b0d7d1c0a3
Replit user <>
2022-10-07 10:31:10 +0000

    select

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-browser = webdriver.Chrome(options=options)
-
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
-browser.get(f"{base_url}{search_term}")
-response = browser.page_source
-
-soup = BeautifulSoup(response, features="html5lib")
-
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
-jobs = job_list.find_all("li", recursive=False)
-for job in jobs:
-  zone = job.find("div", class_="mosaic-zone")
-  if zone == None:
-    anchor = job.select_one("h2 a")
-    title = anchor['aria-label']
-    link = anchor['href']
-    print(title, link)
-    print("///")
+def extract_indeed_jobs(keyword):…
