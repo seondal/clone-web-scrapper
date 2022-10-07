bce032bc47f5c006f77e47b07cf0337b8b41c3dd
seondal <sseondal@gmail.com>
2022-10-07 17:49:29 +0900

    wait

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"}
 
-search_term="react"
-url = f"https://remoteok.com/remote-{search_term}-jobs"
-response = get(url, headers=headers)
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}", headers=headers)
 
 if response.status_code != 200:
-  print("Can't request website")
+    print("Cant request page")
 else:
-  soup = BeautifulSoup(response.text, "html.parser")
-  jobs = soup.find_all("td", class_="company position company_and_position")
-  jobs.pop(0)
-  results = []
-  for job in jobs:
-    title = job.find("h2", itemprop="title")
-    name = job.find("h3", itemprop="name")
-    tags = job.find_all("div", class_="location")
-    tag_data = []
-    for tag in tags:
-      tag_data.append(tag.string)
-    job_data = {
-      "title": title.string,
-      "name": name.string,
-      "tags": tag_data
-    }
-    results.append(job_data)
-
-  for result in results:
-    print(result)
+  print(response.text)
