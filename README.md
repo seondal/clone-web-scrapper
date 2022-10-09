7ac52799762670f975562b136d4c8a13b8ca71cc
Replit user <>
2022-10-09 19:35:10 +0000

    feat: file download & doc: ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# web-scrapper
- 파이썬이랑 친해져보자
+[Link](https://replit.com/@seondal/clone-web-scrapper#README.md)
+
+[ReadMe](https://suave-lilac-075.notion.site/Web-Scrapper-47c806a9209244dbab6af933ca63286e)
+
+---
+
+### 실행 방법
+1. 위쪽 초록색 Play 버튼을 클릭하여 서버를 구동합니다.
+2. [https://clone-web-scrapper.seondal.repl.co/](https://clone-web-scrapper.seondal.repl.co/)로 접속합니다.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.wwr import extract_wwr_jobs
 from extractors.remoteok import extract_remoteok_jobs
+# from extractors.indeed import extract_indeed_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -22,9 +24,21 @@ def search():
   else:
     remoteok = extract_remoteok_jobs(keyword)
     wwr = extract_wwr_jobs(keyword)
+    # indeed = extract_indeed_jobs(keyword)
     jobs = remoteok + wwr
     db[keyword] = jobs
   return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")  # replit 에서 실행할 것이므로
